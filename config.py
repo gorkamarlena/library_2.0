70af7db3bc48de153c208278db380850c557cf57
Marlena Gorka <marlenagorka.mg@gmail.com>
2023-03-21 23:08:34 +0100

    add admin username & password

--- config.py
+++ config.py
@@ -1,12 +1,13 @@
-# config.py
 import os
 
-BASE_DIR = os.path.abspath(os.path.dirname(__file__))    # 1
+BASE_DIR = os.path.abspath(os.path.dirname(__file__))    
 
 class Config:
-   SECRET_KEY = os.environ.get("SECRET_KEY") or "remember-to-add-secret-key"    # 2
-   SQLALCHEMY_DATABASE_URI = (                           # 3
+   SECRET_KEY = os.environ.get("SECRET_KEY") or "remember-to-add-secret-key"    
+   SQLALCHEMY_DATABASE_URI = (                           
            os.environ.get('DATABASE_URL') or
-           'sqlite:///' + os.path.join(BASE_DIR, 'mikroblog.db')
+           'sqlite:///' + os.path.join(BASE_DIR, 'library.db')
    )
    SQLALCHEMY_TRACK_MODIFICATIONS = False
+   ADMIN_USERNAME = os.environ.get("ADMIN_USERNAME", "admin")
+   ADMIN_PASSWORD = os.environ.get("ADMIN_PASSWORD", "change-me")
